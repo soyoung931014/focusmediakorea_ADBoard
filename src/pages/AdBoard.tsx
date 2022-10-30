3ab28ab6f6bf507a19b3c8f3f79f549da7697b29
park_so_young <soyoung931014@gmail.com>
2022-10-30 23:52:11 +0900

    design: 레이아웃 및 css

--- src/pages/AdBoard.tsx
+++ src/pages/AdBoard.tsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const AdBoard = () => {
-  return <div>AdBoard</div>;
+  return (
+    <Container>
+      <TitleWrpper>
+        <Title>title작성합니다.</Title>
+      </TitleWrpper>
+      <ImgWrapper>
+        <Img
+          alt="mockAdImg"
+          src={`${process.env.PUBLIC_URL}/images/ad_image.png`}
+        ></Img>
+      </ImgWrapper>
+      <CodeWrapper>
+        <div>qr코드</div>
+      </CodeWrapper>
+    </Container>
+  );
 };
 
 export default AdBoard;
+
+const Container = styled.div`
+  height: 100vh;
+`;
+const TitleWrpper = styled.div`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Title = styled.div`
+  font-size: 20px;
+`;
+const ImgWrapper = styled.div``;
+const Img = styled.img`
+  width: 44.9rem;
+  height: 90vh;
+  border: solid red 2px;
+`;
+const CodeWrapper = styled.div`
+  z-index: 10;
+  position: sticky;
+
+  left: 0;
+  font-size: 50px;
+`;
