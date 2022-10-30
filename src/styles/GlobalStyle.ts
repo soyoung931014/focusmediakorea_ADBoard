00cadaed55dd6caabd392d55e72fac272872f44b
park_so_young <soyoung931014@gmail.com>
2022-10-30 22:35:45 +0900

    feat: style추가

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+    ${reset}
+    * {
+    box-sizing: border-box;
+    outline: none;
+    border: none;
+  }
+  body {
+    color: #000;
+    background-color:#fff;
+  }
+  button {
+    cursor: pointer;
+  }
+`;
+export default GlobalStyle;
---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+const color = {
+  grey_01: '#6A697C',
+};
+const deviceSizes = {
+  minSize: '720px',
+};
+const theme = {
+  color,
+  deviceSizes,
+};
+
+export default theme;
