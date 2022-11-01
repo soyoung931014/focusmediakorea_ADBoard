2217e376931dc6b758ea390494b14b994b8422d9
park_so_young <soyoung931014@gmail.com>
2022-11-01 20:06:25 +0900

    style: try,catch 추가

--- src/api/adApi.ts
+++ src/api/adApi.ts
@@ -2,8 +2,12 @@ import { BASE_URL } from './index';
 import axios from 'axios';
 
 export const fetchAd = async (today: string, currentTime: string | number) => {
-  const response = await axios.get(
-    `${BASE_URL}/adInfo?announcementDay=${today}&startTime=${currentTime}:00`,
-  );
-  return response.data;
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/adInfo?announcementDay=${today}&startTime=${currentTime}:00`,
+    );
+    return response.data;
+  } catch (error) {
+    return console.log(error);
+  }
 };
