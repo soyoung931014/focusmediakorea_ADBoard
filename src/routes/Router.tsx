4fe2978b3e221e89b08df8c1b9de5fdc5143c9bc
park_so_young <soyoung931014@gmail.com>
2022-11-06 01:56:02 +0900

    feat: code splitting

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router';
-import Layout from '../layout/Layout';
-import AdBoard from '../pages/AdBoard';
-import InfoRegister from '../pages/InfoRegister';
+
+const Layout = React.lazy(() => import('../layout/Layout'));
+const AdBoard = React.lazy(() => import('../pages/AdBoard'));
+const InfoRegister = React.lazy(() => import('../pages/InfoRegister'));
+
 const Router = () => {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<AdBoard />} />
-        <Route path="/infoRegister/:adId" element={<InfoRegister />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<div>loading...</div>}>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<AdBoard />} />
+          <Route path="/infoRegister/:adId" element={<InfoRegister />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
