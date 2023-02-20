fe1fdd678ba9b46bb1a4318f87725c64de73f161
park_so_young <80194405+soyoung931014@users.noreply.github.com>
2023-02-20 16:59:01 +0900

    Refactoring: 광고 마감기한 추가 및 데이터 페칭 로직 변경 (#9)
    * style: 오타 수정
    
    * refactoring: 데이터 수정 및 기간별 데이터페칭 로직수정
    
    * fix: 오타수정
    
    * style: 목 데이터 추가

--- src/api/adApi.ts
+++ src/api/adApi.ts
@@ -3,12 +3,27 @@ import axios from 'axios';
 
 export const fetchAd = async (today: string, currentTime: string | number) => {
   try {
+    const criteriaDay = new Date(today).getTime();
+
     const response = await axios.get(
-      `${BASE_URL}/adInfo?announcementDay=${today}&startTime=${currentTime}:00`,
+      `${BASE_URL}/adInfo?startTime=${currentTime}:00`,
     );
-    if (response.data.length === 0) {
-      return;
+
+    if (response.data.length === 0) return;
+
+    const { data } = response;
+    const filteredDate = [];
+    for (const ad of data) {
+      const { announcementDay, EndDay } = ad;
+      const start = new Date(announcementDay).getTime();
+      const end = new Date(EndDay).getTime();
+      if (start <= criteriaDay && criteriaDay <= end) {
+        filteredDate.push(ad);
+      }
     }
+
+    response.data = [...filteredDate];
+
     return response.data;
   } catch (error) {
     return console.log(error);
